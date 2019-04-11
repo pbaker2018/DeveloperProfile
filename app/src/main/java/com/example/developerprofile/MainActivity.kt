31db7791efe76650141b5f7b803910cfa31f817b
Pauline <pbaker@hotmail.co.uk>
2019-04-11 16:45:35 +0100

    Added four main images and the constraints for different screen sizes

--- app/src/main/java/com/example/developerprofile/MainActivity.kt
+++ app/src/main/java/com/example/developerprofile/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.developerprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
+        // you could also do rounded.isCircular = true
+        // which would turn the logo into a circle
+        // the 4 lines of code above give the square logo rounded corners
+
     }
 }
